744ee0148ebbe3c4cca77634362feac6726dcca7
Sebastián <s.ruizdiaz96@gmail.com>
2022-11-14 16:29:03 -0300

    Comentarios de arrows

--- src/es6/01-arrow-functions.js
+++ src/es6/01-arrow-functions.js
@@ -1,8 +1,12 @@
+
+//Función normal
 function square(num) {
     return num * num;
 }
+
+// Función con arrow
 const square = (num) => {
     return num * num;
 }
-
+// Función con arrow simplificada
 const square = num => num * num ;
