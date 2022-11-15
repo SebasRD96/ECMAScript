28b8c5249fa6345e0f386d513785fda3e4e6fef2
Sebastián <s.ruizdiaz96@gmail.com>
2022-11-15 15:59:56 -0300

    Ejemplo de set

--- src/es6/10-set-add.js
+++ src/es6/10-set-add.js
@@ -4,3 +4,17 @@ list.add('item 1');
 list.add('item 2').add('item 3');
 
 console.log(list);
+
+//Ejemplo de uso eliminando elementos repetidos en un array
+
+const arr = [1, 1, 2, 2, 3, 4, 4, 5];
+
+const set = new Set(arr);
+
+const arrSinRep = Array.from(set)
+
+const sinRepetidos = [...arrSinRep];
+
+console.log(arr);
+console.log(arrSinRep);
+console.log(sinRepetidos);
