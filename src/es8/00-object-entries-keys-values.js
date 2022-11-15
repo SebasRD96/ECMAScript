9254436f26a5d831f3d943a540d960364b7fdf89
Sebastián <s.ruizdiaz96@gmail.com>
2022-11-15 16:20:24 -0300

    ES8, objetos

---
+++ src/es8/00-object-entries-keys-values.js
@@ -0,0 +1,6 @@
+
+const countries = {MX: 'Mexico', CO: 'Colombia', ARG: 'Argentina', CL: 'Chile', PE: 'Peru'};
+console.log(Object.entries(countries));
+
+console.log(Object.keys(countries));
+console.log(Object.values(countries));
