6eddad03a432bb045225ac488612e25bb90cac6d
Sebastián <s.ruizdiaz96@gmail.com>
2022-11-14 23:12:01 -0300

    Spread Operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -12,3 +12,22 @@ console.log(a, fruits[1]);
 let user = {username: 'Sebastián', age: 26};
 let {username, age} = user;
 console.log(username, user.age);
+
+
+//Spread operator
+
+let person = {name: 'Sebastián', age: 26};
+let country = 'ARG';
+
+let data = {id: 1, ...person, country};
+console.log(data);
+
+//rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
