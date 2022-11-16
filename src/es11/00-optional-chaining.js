bfdcec53e9846472b753d36510e6fec4e675046e
Sebastián <s.ruizdiaz96@gmail.com>
2022-11-16 19:25:00 -0300

    ES11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,11 @@
+const users = {
+    srd: {
+        country: 'ARG'
+    },
+    ana: {
+        country:'CO'
+    }
+}
+
+
+console.log(users?.bebeloper?.country)
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 8907245920742093847n;
+const anotherBigNumber = BigInt(328173019832190320);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
